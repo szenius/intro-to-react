177e5a514ef0826de1a0bf838ec83ee4fdbd1114
Sze Ying <ting.szeying@gmail.com>
2021-08-03 22:35:48 +0800

    use regular functions

--- props-and-state/src/components/Counter.jsx
+++ props-and-state/src/components/Counter.jsx
@@ -4,7 +4,13 @@ function Counter(props) {
   return (
     <div>
       <p>You clicked {props.count} times</p>
-      <button onClick={() => props.setCount(props.count + 1)}>Click me</button>
+      <button
+        onClick={function () {
+          props.setCount(props.count + 1);
+        }}
+      >
+        Click me
+      </button>
     </div>
   );
 }

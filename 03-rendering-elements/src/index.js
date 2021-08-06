8a014119f4c27602e0044b8a3a283b66dd0ff039
dzldebby <the.idiosyncrasies@gmail.com>
2021-08-06 12:01:54 +0800

    refactor: move edits to app.js

--- 03-rendering-elements/src/index.js
+++ 03-rendering-elements/src/index.js
@@ -4,11 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const name = "Debby"
-const hello = <h1>Hello, my name is {name}!</h1>
-
-
-ReactDOM.render(hello,
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
